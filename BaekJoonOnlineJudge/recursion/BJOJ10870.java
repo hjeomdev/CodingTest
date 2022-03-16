dad154b7bfec2b64965dca8c88cd6ac81621463e
Hyejin Eom <hjeom.dev@gmail.com>
2022-03-16 23:54:21 +0900

    [Recursion] 피보나치(BJOJ10870)

---
+++ BaekJoonOnlineJudge/recursion/BJOJ10870.java
@@ -0,0 +1,33 @@
+// 피보나치 
+// 8분
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BJOJ10870 {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		int result = fibonaci(n);
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(result + "\n");
+		bw.flush();
+		bw.close();
+	}
+
+	public static int fibonaci(int n){
+		if(n == 0) {
+			return 0;
+		} else if(n == 1) {
+			return 1;
+		} 
+
+		return fibonaci(n - 2) + fibonaci(n - 1);
+	}
+}
