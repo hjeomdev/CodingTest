aadc4b6b539a97d59612c7fffd4c36776e15a3d2
Hyejin Eom <hjeom.dev@gmail.com>
2022-08-03 16:34:38 +0900

    [BOJ] P11729.py / 하노이 탑 이동 순서 / 1시간

---
+++ BOJ/P11729.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+def hanoi(n, a, b, c): # n개의 원반을 a에서 c로 이동하는데 b를 보조로 사용해라.
+    if n == 1:
+        print(a, c) # a에 있는 원반을 c로 이동
+    else:
+        hanoi(n - 1, a, c, b) # 가장 아래의 원반을 제외한 n-1개의 원반을 a에서 b로 옮긴다.
+        print(a, c) # a에 있는 원반을 c로 이동
+        hanoi(n - 1, b, a, c) # n-1개의 원반을 b에서 c로 옮긴다.
+
+# 하노이는 여전히 이해가 될듯 하면서도 안된다.. 이론은 이해가 가도 코드로 도출하는게 어렵다.
+# 실제로 이런 문제가 나오면 풀 수 있을까?
+
+def hanoi_total_count(n):
+    if n == 1:
+        print(1)
+    elif n == 2:
+        print(3)
+    else:
+        print(n * 2 + 1)
+
+hanoi_total_count(n)
+hanoi(n, 1, 2, 3)
