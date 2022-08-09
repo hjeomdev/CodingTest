b120f530ec05f325b054f0438deacf4ecc768120
Hyejin Eom <hjeom.dev@gmail.com>
2022-08-09 21:03:29 +0900

    remove: all legacy file, modify: gitignore(jetbrain, masOS, java)

--- PGS/70128.py
+++
@@ -1,8 +0,0 @@
-# https://programmers.co.kr/learn/courses/30/lessons/70128
-# 2022. 05. 26 / 1분
-
-def solution(a, b):
-    answer = 0
-    for a, b in zip(a, b):
-        answer += a * b
-    return answer
