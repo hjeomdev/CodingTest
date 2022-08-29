e41a88bb761408c381104760843bc8b0b95f6c2e
Hyejin Eom <hjeom.dev@gmail.com>
2022-08-29 19:38:13 +0900

    [BOJ] P11047.py / 동전 0 / 10분

---
+++ python/BOJ/P11047.py
@@ -0,0 +1,21 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().rstrip().split())
+coins = []
+for _ in range(n):
+    coins.append(int(sys.stdin.readline().rstrip()))
+
+total = 0
+for i in range(len(coins) - 1, -1, -1):
+    # print(coins[i])
+    if k == 0:
+        break
+
+    if k >= coins[i]:
+        temp = k // coins[i]
+        # print('temp', temp, coins[i])
+        k -= coins[i] * temp
+        total += temp
+        # print(total)
+
+sys.stdout.write(str(total))
